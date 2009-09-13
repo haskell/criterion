ef4310b22e358c1c71592421feb41c85a68e88b3
Bryan O'Sullivan <bos@serpentine.com>
2009-09-13 01:49:49 +0000

    Get a build sort of working.
    --HG--
    extra : convert_revision : eef781587a1c107acc62f62525af2e4165127fa1

--- VectorUtil.hs
+++ VectorUtil.hs
@@ -6,6 +6,17 @@ import Control.Monad.ST
 import Data.Array.Vector
 import System.Random.Mersenne
 
+-- | Create an array, using the given action to populate each element.
+createU :: (UA e) => Int -> (Int -> IO e) -> IO (UArr e)
+createU size itemAt = assert (size >= 0) $
+    unsafeSTToIO (newMU size) >>= loop 0
+  where
+    loop k arr | k >= size = unsafeSTToIO (unsafeFreezeAllMU arr)
+               | otherwise = do
+      r <- itemAt k
+      unsafeSTToIO (writeMU arr k r)
+      loop (k+1) arr
+
 randomU :: (UA e, MTRandom e) => Int -> MTGen -> IO (UArr e)
 randomU n gen = createU n . const $ random gen
 
