d5c43653473f94a606c8f6b1144d20e4a9fe0dd4
Bryan O'Sullivan <bos@serpentine.com>
2011-11-18 21:53:35 -0800

    Make the tiny example less tiny.

--- examples/Tiny.hs
+++ examples/Tiny.hs
@@ -9,10 +9,12 @@ import Criterion.Config
 main = defaultMainWith defaultConfig (return ()) [
          bgroup "fib" [
            bench "fib 10" $ whnf fib 10
+         , bench "fib 20" $ whnf fib 20
          , bench "fib 30" $ whnf fib 30
          ],
          bgroup "intmap" [
-           bench "intmap 50k" $ whnf intmap 50000
+           bench "intmap 25k" $ whnf intmap 25000
+         , bench "intmap 50k" $ whnf intmap 50000
          , bench "intmap 75k" $ whnf intmap 75000
          ]
        ]
