0c40072bdb7e9af186e390e5a0673116c3676d5e
Bryan O'Sullivan <bos@serpentine.com>
2014-03-14 17:01:47 -0700

    Add an initial version of the conduit-vs-pipes benchmark
    This exhibits the undesirable behaviour described in gh-35.
    
    --HG--
    extra : amend_source : 265970a8d88f3b7cd4f68bc937af0209153c6c5f

---
+++ examples/ConduitVsPipes.hs
@@ -0,0 +1,27 @@
+-- Contributed by Gabriel Gonzales as a test case for
+-- https://github.com/bos/criterion/issues/35
+
+import Criterion.Main
+import Data.Conduit
+import Data.Functor.Identity
+import Pipes
+import qualified Data.Conduit.List as C
+import qualified Pipes.Prelude as P
+
+criterion :: Int -> IO ()
+criterion n = defaultMain
+    [ bgroup "IO"
+        [ bench "pipes"   $ nfIO (pipes   n)
+        , bench "conduit" $ nfIO (conduit n)
+        ]
+    , bgroup "Identity"
+        [ bench "pipes"   $ whnf (runIdentity . pipes  ) n
+        , bench "conduit" $ whnf (runIdentity . conduit) n
+        ]
+    ]
+
+pipes, conduit :: (Monad m) => Int -> m ()
+pipes n = runEffect $ for (each [1..n] >-> P.map (+1) >-> P.filter even) discard
+conduit n = C.sourceList [1..n] $= C.map (+1) $= C.filter even $$ C.sinkNull
+
+main = criterion (10^5)
