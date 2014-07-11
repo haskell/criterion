c69e03a4a405e6f581d2c5b0fa86b4579179e6f6
Bryan O'Sullivan <bos@serpentine.com>
2014-07-12 00:39:22 +0100

    Impose an upper limit on how long the sanity test runs

--- tests/Sanity.hs
+++ tests/Sanity.hs
@@ -1,4 +1,7 @@
-import Criterion.Main
+import Criterion.Main (bench, defaultMain, whnf)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import System.Timeout (timeout)
 
 fib :: Int -> Int
 fib 0 = 0
@@ -6,4 +9,13 @@ fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
 main :: IO ()
-main = defaultMain [ bench "fib 10" $ whnf fib 10 ]
+main = do
+  let tooLong = 30
+  wat <- timeout (tooLong * 1000000) $
+         defaultMain [ bench "fib 10" $ whnf fib 10 ]
+  case wat of
+    Just () -> return ()
+    Nothing -> do
+      hPutStrLn stderr $ "*** killed for running longer than " ++
+                         show tooLong ++ " seconds!"
+      exitFailure
