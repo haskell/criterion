6cc263441ece30d3d3e4445034fab2e06226a1ea
Bryan O'Sullivan <bos@serpentine.com>
2014-07-14 20:33:48 -0700

    Recast sanity test as a HUnit test, to silence Jenkins
    --HG--
    extra : amend_source : 6cdc037629abaf671826365ec2f784bd45693ba3

--- tests/Sanity.hs
+++ tests/Sanity.hs
@@ -1,7 +1,13 @@
-import Criterion.Main (bench, defaultMain, whnf)
-import System.Exit (exitFailure)
-import System.IO (hPutStrLn, stderr)
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Criterion.Main (bench, whnf)
+import System.Environment (getEnv, withArgs)
 import System.Timeout (timeout)
+import Test.Framework (defaultMain)
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit (Assertion, assertFailure)
+import qualified Criterion.Main as C
+import qualified Control.Exception as E
 
 fib :: Int -> Int
 fib = sum . go
@@ -9,15 +15,23 @@ fib = sum . go
         go 1 = [1]
         go n = go (n-1) ++ go (n-2)
 
+sanity :: Assertion
+sanity = do
+  args <- getArgEnv
+  withArgs args $ do
+    let tooLong = 30
+    wat <- timeout (tooLong * 1000000) $
+           C.defaultMain [ bench "fib 10" $ whnf fib 10
+                         , bench "fib 22" $ whnf fib 22 ]
+    case wat of
+      Just () -> return ()
+      Nothing -> assertFailure $ "killed for running longer than " ++
+                                 show tooLong ++ " seconds!"
+
 main :: IO ()
-main = do
-  let tooLong = 30
-  wat <- timeout (tooLong * 1000000) $
-         defaultMain [ bench "fib 10" $ whnf fib 10
-                     , bench "fib 22" $ whnf fib 22 ]
-  case wat of
-    Just () -> return ()
-    Nothing -> do
-      hPutStrLn stderr $ "*** killed for running longer than " ++
-                         show tooLong ++ " seconds!"
-      exitFailure
+main = defaultMain [testCase "sanity" sanity]
+
+getArgEnv :: IO [String]
+getArgEnv =
+  fmap words (getEnv "ARGS") `E.catch`
+  \(_ :: E.SomeException) -> return []
