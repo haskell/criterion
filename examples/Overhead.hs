2fce441f0244beea5cd3c856172051932b3e616b
Bryan O'Sullivan <bos@serpentine.com>
2014-08-07 11:37:07 -0700

    Add a benchmark for measuring measurement overhead

---
+++ examples/Overhead.hs
@@ -0,0 +1,27 @@
+-- This benchmark measures the timing overhead added by the various
+-- functions we use to measure performance.
+
+{-# LANGUAGE CPP #-}
+
+module Main (main) where
+
+import Criterion.Main
+import Criterion.Measurement as M
+import GHC.Stats as GHC
+
+main :: IO ()
+main = do
+  statsEnabled <- getGCStatsEnabled
+  defaultMain $ [
+      bench "getTime" $      whnfIO M.getTime
+    , bench "getCPUTime" $   whnfIO M.getCPUTime
+    , bench "getCycles" $    whnfIO M.getCycles
+    , bench "M.getGCStats" $ whnfIO M.getGCStats
+    ] ++ if statsEnabled
+         then [bench "GHC.getGCStats" $ whnfIO GHC.getGCStats]
+         else []
+
+#if !MIN_VERSION_base(4,6,0)
+getGCStatsEnabled :: IO Bool
+getGCStatsEnabled = return False
+#endif
