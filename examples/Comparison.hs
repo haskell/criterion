c84c70df40780a999ef1c5c7392d739caf0e0c5f
Neil Mitchell <ndmitchell@gmail.com>
2015-01-04 17:46:28 +0000

    Fix the Comparison example, since bcompare was deleted

--- examples/Comparison.hs
+++ examples/Comparison.hs
@@ -1,9 +1,7 @@
 import Criterion.Main
 
 main = defaultMain [
-   bcompare [
-     bench "exp" $ whnf exp (2 :: Double)
-   , bench "log" $ whnf log (2 :: Double)
-   , bench "sqrt" $ whnf sqrt (2 :: Double)
-   ]
+   bench "exp" $ whnf exp (2 :: Double)
+ , bench "log" $ whnf log (2 :: Double)
+ , bench "sqrt" $ whnf sqrt (2 :: Double)
  ]
