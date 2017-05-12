1a4d85ac216d3000ddb06e89eff1cf455525a9cf
Ryan Scott <ryan.gl.scott@gmail.com>
2017-05-12 09:25:31 -0400

    Revert "Update the tests for statistics-0.14.  Also switch from submodule to location: stanza"
    This reverts commit b881b35900af739f485667cf0bdeb2154bb0a25c.
    
    This is no longer needed, since statistics-0.14 is on Hackage.

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -7,8 +7,8 @@ import qualified Data.Aeson as Aeson
 import qualified Data.Vector as V
 import Properties
 import Statistics.Types (estimateFromErr, mkConfLevel)
-import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.HUnit (testCase)
+import Test.Framework (defaultMain)
+import Test.Framework.Providers.HUnit
 import Test.HUnit
 
 r1 :: Report
@@ -16,7 +16,7 @@ r1 = Report 0 "" [] v1 s1 (Outliers 0 0 0 0 0) []
  where
   m1 = Measured 4.613000783137977e-05 3.500000000000378e-05 31432 1 0 0 0 0.0 0.0 0.0 0.0
   v1 = V.fromList [m1]
-  est1 = estimateFromErr 0.0 (0.0, 0.0) (mkConfLevel 0.0)
+  est1 = Estimate 0.0 0.0 0.0 0.0
   s1 = SampleAnalysis [] 0.0 est1 est1 (OutlierVariance Unaffected "" 0.0)
 
 m2 :: Measured
