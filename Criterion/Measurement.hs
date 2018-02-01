d957c8224a32e8ac4fc542e9941b8e9e2f0a87e7
Patrick Dougherty <patrick.doc@ameritech.net>
2018-02-01 16:55:19 -0600

    Fix per-run combine time (fixes #181) (#182)
    * Fix per-run combine time
    
    * Document change

--- Criterion/Measurement.hs
+++ Criterion/Measurement.hs
@@ -196,8 +196,10 @@ measure bm iters = runBenchmarkable bm iters addResults $ \ !n act -> do
            }
   return (m, endTime)
   where
+    -- When combining runs, the Measured value is accumulated over many runs,
+    -- but the Double value is the most recent absolute measurement of time.
     addResults :: (Measured, Double) -> (Measured, Double) -> (Measured, Double)
-    addResults (!m1, !d1) (!m2, !d2) = (m3, d1 + d2)
+    addResults (!m1, _) (!m2, !d2) = (m3, d2)
       where
         add f = f m1 + f m2
 
